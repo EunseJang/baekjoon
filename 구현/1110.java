159dd94682f57472701f3d1c051f9fedfe4b3fd8
Eunse Jang <senuej37@gmail.com>
2024-03-05 13:48:14 +0900

    [24.03.05] 더하기 사이클

---
+++ 구현/1110.java
@@ -0,0 +1,20 @@
+package 구현;
+
+import java.util.Scanner;
+
+class _1110 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int count = 1;
+    int tmp = (N%10*10) + ((N/10 + N%10)%10);;
+
+    while (N != tmp) {
+      tmp = (tmp%10*10) + ((tmp/10 + tmp%10)%10);
+      count ++;
+    }
+
+    System.out.println(count);
+  }
+}
