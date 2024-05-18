4cdacd1ba4ee992f56d36373bc858899fb929800
Eunse Jang <senuej37@gmail.com>
2024-05-18 14:56:24 +0900

    [24.05.18] 구간 합 구하기 5

--- array/11660.py
+++ array/11660.py
@@ -8,8 +8,6 @@ for i in range(N):
     row = list(map(int, input().split()))
     matrix.append(row)
 
-
-# 합 배열 만들기
 sumMatrix = [[0] * (N+1) for _ in range(N+1)]
 for i in range(1, N+1):
     for j in range(1, N+1):
